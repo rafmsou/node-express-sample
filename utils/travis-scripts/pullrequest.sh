9967232bc9bd5d137ecc24f2815f2c5101407cb0
Rafael Sousa <rafael@Mac-Rafmsou.local>
2016-03-15 22:12:23 -0300

    Test Travis

---
+++ utils/travis-scripts/pullrequest.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+
+#The -e flag causes the script to exit as soon as one command returns a non-zero exit code.
+#The -v flag makes the shell print all lines in the script before executing them
+set -ev
+
+npm run lint
