8cb99f953d613ff195f52a5aa7c4883e79daedba
Rafael Sousa <rafael@Mac-Rafmsou.local>
2016-03-15 22:27:11 -0300

    Shebang

--- utils/travis-scripts/pullrequest.sh
+++ utils/travis-scripts/pullrequest.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 
 #The -e flag causes the script to exit as soon as one command returns a non-zero exit code.
 #The -v flag makes the shell print all lines in the script before executing them
